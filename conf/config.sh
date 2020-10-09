836270b033404013bdf0167393c695dacc12b4de
CloudSen <www.402645063@gmail.com>
2020-10-09 16:11:55 +0800

    chore(confi): 配置文件添加注释说明
    配置文件添加注释说明

--- conf/config.sh
+++ conf/config.sh
@@ -16,7 +16,7 @@ password=""
 ## root password
 rootPassword=""
 
-# Where to install grub
+# Where to install grub, u can check by "fdisk --list"
 grubDevice="/dev/sda"
 
 # Automically Partition
@@ -31,9 +31,13 @@ autoPartitionDevice="/dev/sda"
 homePartitionSize="+10G"
 rootPartitionSize="+20G"
 
-# What u want to install, separated by space.
+# Whether install graphic environment
 enableGraphic=true
+# Essential soft, separated by space.
+# If using Intel CPU, then specify "intel-ucode", If using AMD CPU, then specify amd-ucode
 coreSoft="vim dialog wpa_supplicant ntfs-3g networkmanager network-manager-applet screenfetch intel-ucode grub"
+# What u want to install, separated by space.
 dailySoft="git"
+# If u want smallest system, then remove "kde-applications"
 graphicEnvironment="xorg plasma kde-applications sddm"
 vmTools="gtkmm3 open-vm-tools xf86-input-vmmouse xf86-video-vmware"
